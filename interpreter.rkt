521e16785ba3bd94da5da8aea8a913a93217b7da
Kim <kaa97@case.edu>
2018-02-14 23:27:03 -0500

    Can return boolean values

--- interpreter.rkt
+++ interpreter.rkt
@@ -12,6 +12,10 @@
 ;if statement	(if conditional then-statement optional-else-statement)
 ;while statement	(while conditional body-statement)
 
+
+;Problem:
+;returns #t instead of "true"
+
 #lang racket
 
 (require "simpleParser.scm")
@@ -33,6 +37,7 @@
 (define Mstate_stmt
   (lambda (stmt s)
     (cond
+      ((null? stmt) s)
       ((eq? 'return (operator stmt)) (Mstate_return stmt s))
       ((or (eq? '+ (operator stmt))
            (eq? '- (operator stmt))
@@ -45,7 +50,8 @@
            (eq? '<= (operator stmt))
            (eq? '== (operator stmt))
            (eq? '!= (operator stmt))) (Mboolean stmt s))
-      ((eq? 'if (operator stmt)) (Mstate_if (first_operand stmt) (second_operand stmt) (cadddr stmt) s))
+      ((and (eq? 'if (operator stmt)) (null? (cdddr stmt))) (Mstate_if (first_operand stmt) (second_operand stmt) null s))
+      ((eq? 'if (operator stmt)) (Mstate_if (first_operand stmt) (second_operand stmt) (third_operand stmt) s))
       ((eq? 'while (operator stmt)) (Mstate_while (first_operand stmt) (second_operand stmt) s))
       ((eq? '= (operator stmt)) (Mstate_assign (first_operand stmt) (second_operand stmt) s))
       ((and (eq? 'var (operator stmt)) (null? (cddr stmt))) (Mstate_declare (first_operand stmt) s))
@@ -72,9 +78,9 @@
 ;returns the state after a declaration
 (define Mstate_declare
   (lambda (variable s)
-    (cond
-      ((M_
-    (add_binding variable null s)))
+    (if (variable_declared? variable (name_list s))
+        (raise 'Variable-already-declared)
+        (add_binding variable null s))))
 
 ;returns the state after an assignment
 (define Mstate_assign
@@ -105,33 +111,45 @@
       ((eq? '== (operator condition))(eq? (Mvalue (first_operand condition) s) (Mvalue (second_operand condition) s)))
       ((eq? '!= (operator condition))(not (eq? (Mvalue (first_operand condition) s) (Mvalue (second_operand condition) s))))
       ((eq? '< (operator condition))(< (Mvalue (first_operand condition) s) (Mvalue (second_operand condition) s)))
-      ((eq? '< (operator condition))(< (Mvalue (first_operand condition) s) (Mvalue (second_operand condition) s)))
       ((eq? '<= (operator condition))(<= (Mvalue (first_operand condition) s) (Mvalue (second_operand condition) s)))
       ((eq? '>= (operator condition))(>= (Mvalue (first_operand condition) s) (Mvalue (second_operand condition) s)))
       ((eq? '> (operator condition))(> (Mvalue (first_operand condition) s) (Mvalue (second_operand condition) s))))))
+
 
 ;returns the value of 'value' with the current state 's'
 (define Mvalue
   (lambda (value s)
     (cond
-      ((null? s) 'Error)
+      ((null? s) (raise 'Value-does-not-exist))
       ((number? value) value)
       ((boolean? value) value)
       ((eq? 'true value) #t)
       ((eq? 'false value) #f)
       ((not(pair? value)) (Mvalue_var value s))
-      ((eq? '+ (operator value))(+ (Mvalue (first_operand value) s) (Mvalue (second_operand value)s)))
-      ((eq? '- (operator value))(- (Mvalue (first_operand value) s) (Mvalue (second_operand value)s)))
+      ((eq? '+ (operator value)) (+ (Mvalue (first_operand value) s) (Mvalue (second_operand value) s)))
+      ((and (eq? '- (operator value)) (null? (cddr value)))(- 0 (Mvalue (first_operand value) s)))
+      ((eq? '- (operator value)) (- (Mvalue (first_operand value) s) (Mvalue (second_operand value)s)))
       ((eq? '* (operator value))(* (Mvalue (first_operand value) s) (Mvalue (second_operand value)s)))
       ((eq? '/ (operator value))(quotient(Mvalue (first_operand value) s) (Mvalue (second_operand value)s)))
-      ((eq? '% (operator value))(remainder(Mvalue (first_operand value) s) (Mvalue (second_operand value)s))))))
+      ((eq? '% (operator value))(remainder(Mvalue (first_operand value) s) (Mvalue (second_operand value)s)))
+      ((eq? '&& (operator value)) (and (Mboolean (first_operand value) s) (Mboolean (second_operand value) s)))
+      ((eq? '|| (operator value)) (or (Mboolean (first_operand value) s) (Mboolean (second_operand value) s)))
+      ((or (eq? '> (operator value))
+           (eq? '>= (operator value))
+           (eq? '< (operator value))
+           (eq? '<= (operator value))
+           (eq? '== (operator value))
+           (eq? '!= (operator value))) (Mboolean value s)))))
 
 ;returns the value of the variable with the current state 's'
 (define Mvalue_var
   (lambda (value s)
     (cond
-      ((or (null? value) (null? (name_list s)) (null? (value_list s))) '(123))
-      ((equal? value (car (name_list s))) (car (value_list s)))
+      ((eq? value 'true) #t)
+      ((eq? value 'false) #f)
+      ((or (null? value) (null? (name_list s)) (null? (value_list s))) (raise 'Variable-not-declared))
+      ((and (eq? value (car (name_list s))) (null? (car (value_list s)))) (raise 'Variable-not-assigned-a-value))
+      ((eq? value (car (name_list s)))(car (value_list s)))
       (else (Mvalue_var value (cons (cdr (name_list s)) (cons (cdr (value_list s)) '())))))))
 
 ;checks to see if value is a variable or not
@@ -152,8 +170,17 @@
   (lambda (variable value s)
     (cond
       ((or (null? value) (null? s)) 'Error)
+      ((not (variable_declared? variable (name_list s))) (raise 'Variable-not-declared))
       ((eq? (car (name_list s)) variable) (cons (name_list s) (cons (cons value (cdr (value_list s))) '())))
       (else (add_binding (car (name_list s)) (car (value_list s)) (update_binding variable value (cons (cdr (name_list s)) (cons (cdr (value_list s)) '()))))))))
+
+;returns whether or not the variable is declared in the state s
+(define variable_declared?
+  (lambda (variable lis)
+    (cond
+      ((null? lis) #f)
+      ((eq? variable (car lis)) #t)
+      (else (variable_declared? variable (cdr lis))))))
 
 ;returns the list of names of bindings in a state s
 (define name_list
@@ -173,5 +200,8 @@
 
 ;returns the third element of a list (second operator)
 (define second_operand caddr)
+
+;returns the fourth element of a list (third operator)
+(define third_operand cadddr)
 
 
